374bdfbdf4e25fed83487f624dd1077a704465fa
Lucca Nascimento <luccadev@hotmail.com>
2026-02-18 10:59:40 -0300

    first commit

---
+++ src/main/java/com/lucca/plataforma_de_pagamento/PlataformaDePagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.lucca.plataforma_de_pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDePagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDePagamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucca/plataforma_de_pagamento/PlataformaDePagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucca.plataforma_de_pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDePagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
